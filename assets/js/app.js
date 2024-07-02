8d1ed0065f921a1c5dcfab7264e163a02318aa70
kevin314 <kevlin3141@gmail.com>
2024-07-02 03:36:05 -0500

    Hardcode room name in client (temporarily)

--- assets/js/app.js
+++ assets/js/app.js
@@ -163,7 +163,7 @@ Hooks.Chat = {
       humanBtn.addEventListener('click', () => {
         console.log('HUMAN button clicked');
         prompt.style.display = 'none';
-        if (room === "room:bot") {
+        if (room === "room:930b9c27-5a6d-46e8-bot5-51d998650e40") {
           guessText.textContent = 'BOT!';
         } else {
           guessText.textContent = 'HUMAN!';
